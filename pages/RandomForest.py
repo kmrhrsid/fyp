327c1ef401c3449306dfe548ed2ed2c8f545105e
Kamariah Rasid <94262022+kmrhrsid@users.noreply.github.com>
2025-01-19 17:41:37 +0800

    Update RandomForest.py

--- pages/RandomForest.py
+++ pages/RandomForest.py
@@ -13,9 +13,9 @@ print("Model loaded successfully!")
 st.title("Cardiovascular Risk Prediction")
 
 # Encoding user inputs
-gender = 1 if gender == "Male" else 0
-alco = 1 if alco == "Yes" else 0
-smoke = 1 if smoke == "Yes" else 0
+#gender = 1 if gender == "Male" else 0
+#alco = 1 if alco == "Yes" else 0
+#smoke = 1 if smoke == "Yes" else 0
 cholesterol = {"Normal": 1, "Above Normal": 2, "High": 3}[cholesterol]
 
 gender = st.selectbox("Gender", ["Female", "Male"])

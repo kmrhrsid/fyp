4d5a3b2db4d0effe1b9ccba5aea87b5e6daef387
Kamariah Rasid <94262022+kmrhrsid@users.noreply.github.com>
2025-01-19 23:30:04 +0800

    Update RandomForest.py

--- pages/RandomForest.py
+++ pages/RandomForest.py
@@ -2,22 +2,16 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier  
-# Example model (replace with your model)
 import joblib
 
+# Load the model
 model = joblib.load('random_forest_model (1).pkl')
 print("Model loaded successfully!")
-
 
 # Streamlit app interface
 st.title("Cardiovascular Risk Prediction")
 
-# Encoding user inputs
-#gender = 1 if gender == "Male" else 0
-#alco = 1 if alco == "Yes" else 0
-#smoke = 1 if smoke == "Yes" else 0
-#cholesterol = {"Normal": 1, "Above Normal": 2, "High": 3}[cholesterol]
-
+# User input
 age = st.slider('Age', min_value=20, max_value=80, value=50)
 gender = st.selectbox("Gender", ["Female", "Male"])
 ap_lo = st.number_input("Low Blood Pressure (mmHg)", min_value=0)
@@ -31,16 +25,15 @@ input_data = pd.DataFrame({
     'age': [age],
     'gender': [gender],
     'ap_lo': [ap_lo],
-    'ap_hi': [ap_hi],
+    'ap_hi': [ap_hi],
     'cholesterol': [cholesterol],
     'smoke': [smoke],
     'alco': [alco],
 })
 
-
 # Predict the cardiovascular risk
 if st.button("Submit"):
     prediction = model.predict(input_data)[0]
-    risk = "Risk of Cardiovascular Disease" if prediction[0] == 1 else "No Risk"
+    risk = "Risk of Cardiovascular Disease" if prediction == 1 else "No Risk"
     st.write(f"Prediction: {risk}")
 

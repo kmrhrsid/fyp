58befd7880cfc0a686327dc4adc4f143544a77cb
Kamariah Rasid <94262022+kmrhrsid@users.noreply.github.com>
2025-01-19 16:32:58 +0800

    Update RandomForest.py

--- pages/RandomForest.py
+++ pages/RandomForest.py
@@ -1,17 +1,4 @@
-import streamlit as st
 
-st.title("Simple Streamlit App")
-st.write("Enter your details below:")
-
-name = st.text_input("What's your name?")
-age = st.number_input("What's your age?", min_value=0)
-
-# Blood pressure input
-systolic = st.number_input("Systolic Pressure (mmHg)", min_value=0)
-diastolic = st.number_input("Diastolic Pressure (mmHg)", min_value=0)
-
-if st.button("Submit"):
-    st.success(f"Hello, {name}! You are {age} years old.")
 
 import streamlit as st
 

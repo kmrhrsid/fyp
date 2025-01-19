c102e30dd823e3553066820ffa1379012b8bad1f
Kamariah Rasid <94262022+kmrhrsid@users.noreply.github.com>
2025-01-19 18:17:23 +0800

    Update RandomForest.py

--- pages/RandomForest.py
+++ pages/RandomForest.py
@@ -32,7 +32,7 @@ input_data = np.array([[gender, age, ap_lo, ap_hi, alco, smoke, cholesterol]])
 
 # Predict the cardiovascular risk
 if st.button("Submit"):
-    prediction = model.predict(input_data)
+    prediction = model.predict(input_data)[0]
     risk = "Risk of Cardiovascular Disease" if prediction[0] == 1 else "No Risk"
     st.write(f"Prediction: {risk}")
 

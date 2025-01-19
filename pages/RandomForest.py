4db7e44cc974d8bf2b708313ec481752c9faba19
Kamariah Rasid <94262022+kmrhrsid@users.noreply.github.com>
2025-01-19 16:25:00 +0800

    Update RandomForest.py

--- pages/RandomForest.py
+++ pages/RandomForest.py
@@ -1,25 +1,8 @@
-import streamlit as st 
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix
-import pickle
+import numpy as np
+import streamlit as st
 
-# Initialize and train the Random Forest Classifier
-rf_classifier = RandomForestClassifier(n_estimators=50, max_depth=5, min_samples_split=10, random_state=0) # You can adjust hyperparameters
-#rf_classifier = RandomForestClassifier(n_estimators=100,random_state=0) # You can adjust hyperparameters
-rf_classifier.fit(X_train, y_train)
-
-# Make predictions on the test set
-y_pred = rf_classifier.predict(X_test)
-
-# Evaluate the model
-st.write(classification_report(y_test, y_pred))
-st.write(confusion_matrix(y_test, y_pred))
-
-
-# Save the trained model to a pickle file
-#filename = 'random_forest_model.pkl'pickle.dump(rf_classifier, open(filename, 'wb'))
-with open("random_forest_model.pkl", "wb") as file:
-  pickle.dump(rf_classifier, file)
+df = pd.DataFrame(
+np.random.randn(50, 20),
+columns=('col %d' % i for i in range(20)))
+print(df)

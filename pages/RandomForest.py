f81dc9b91373a23e56d65b56693ffc4006f2a38b
Kamariah Rasid <94262022+kmrhrsid@users.noreply.github.com>
2025-01-22 02:46:40 +0800

    Update RandomForest.py

--- pages/RandomForest.py
+++ pages/RandomForest.py
@@ -97,7 +97,7 @@ if submitted:
     st.subheader("Risk Factor Insights")
     st.write("The following chart shows the relative importance of each feature in predicting cardiovascular risk:")
     fig, ax = plt.subplots()
-    feature_importance_series.plot(kind='bar', ax=ax, color='#FBEAEB')
+    feature_importance_series.plot(kind='bar', ax=ax, color='#7A2048 ')
     ax.set_title("Feature Importance")
     ax.set_ylabel("Importance Score")
     st.pyplot(fig)

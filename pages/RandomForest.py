d36f275dfd27d4e8975fdc30edd35bac4d0773bf
Kamariah Rasid <94262022+kmrhrsid@users.noreply.github.com>
2025-01-24 18:42:29 +0800

    Update RandomForest.py

--- pages/RandomForest.py
+++ pages/RandomForest.py
@@ -42,10 +42,10 @@ def homepage():
     set_background("hp3 (1).jpg")
     st.markdown(
         """
-        <h1 style="font-family: 'Arial', cursive; color:#eca714 ; font-size: 70px; text-align: center;">
+        <h1 style="font-family: 'Arial', cursive; color:#eca714 ; weight='bold'; font-size: 70px; text-align: center;">
         Cardiovascular Risk Prediction
         </h1>
-        <p style="font-family: 'CabinSketch Bold', cursive; color:white ; font-size: 40px; text-align: center;">
+        <p style="font-family: 'CabinSketch Bold', cursive; color:white ; weight='bold'; font-size: 40px; text-align: center;">
         <i>"The greatest wealth is health"</i>
         </p>
         <p style="font-family: 'Arial', cursive; color:white; font-size: 15px; text-align: center;">
